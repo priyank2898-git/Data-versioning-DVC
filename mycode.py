cea8b78795be74040e6171ffda0ac12cca8c078b
priyank2898-git <priyanks2898@gmail.com>
2025-03-02 15:16:08 +0530

    First version commit without DVC

---
+++ mycode.py
@@ -0,0 +1,30 @@
+import pandas as pd 
+import os
+
+#create a dictionary for data fra,e
+data={ 'Name':['Akash','Neeraj','Preetam'],
+        'Age':[20,30,40],
+        'City':['Faridabad','Noida','New delhi']
+}
+
+#craete a Dataframe
+df=pd.DataFrame(data)
+
+#new row
+#new_data1={"Name":"kirti","Age":30,"City":"Ayodhya"}
+#df.loc[len(df.index)] = new_data1
+
+#new row
+#new_data2={"Name":"preeti","Age":24,"City":"Lucknow"}
+#df.loc[len(df.index)] = new_data2
+
+#storing data
+data_dir='data'
+os.makedirs('data',exist_ok=True)
+
+file_path=os.path.join(data_dir,'Sample_data.csv')
+
+#saving the file
+df.to_csv(file_path,index=False)
+
+print(f'The file has been saved to {file_path}')
